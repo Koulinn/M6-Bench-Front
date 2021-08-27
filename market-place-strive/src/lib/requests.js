ad36f97c24052490784ec609c96a64d0e14e0626
Koulinn <drdverzola@gmail.com>
2021-08-27 20:50:22 +0100

    created postFunction
    It can be used thought the app

--- market-place-strive/src/lib/requests.js
+++ market-place-strive/src/lib/requests.js
@@ -4,15 +4,31 @@ const {get} = a
 export const getRequest = async (url)=>{
     try {
         return await get(`${process.env.REACT_APP_URL_PROD}` + url)  
-        
     } catch (error) {
         console.log(error)
     }
+}
 
+export const postJSON = async (url, data, contentType)=>{
+    try {
+        const response = await fetch(`${process.env.REACT_APP_URL_PROD}${url}`,{
+            method: 'post',
+            body: await JSON.stringify({...data}),
+            headers:{
+                ...contentType
+            }
+
+        })
+        const serverData = await response.json()
+        return serverData
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 const request = {
-    getRequest: getRequest
+    getRequest: getRequest,
+    postJSON:postJSON
 }
 
 export default request
