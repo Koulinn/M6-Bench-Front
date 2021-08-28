f22d7bc5cbd5548aae7a9ea6469da4932c19ff0e
Koulinn <drdverzola@gmail.com>
2021-08-28 16:44:14 +0100

    created logic for first load display all products

--- market-place-strive/src/components/views/Home.jsx
+++ market-place-strive/src/components/views/Home.jsx
@@ -3,15 +3,27 @@ import { useEffect, useState } from 'react'
 import CategoriesBar from '../CategoriesBar/CategoriesBar'
 import Hero from '../Hero/Hero'
 import DisplayCards from '../DisplayCards/DisplayCards'
+import { getRequest } from '../../lib/requests'
 
 function Home(props) {
     const [products, setProducts] = useState([])
-    
+    useEffect(() => {
+        try {
+            requestData()
+        } catch (error) {
+            console.log(error)
+        }
+    }, [])
+
+    const requestData = async () => {
+        const getAllProducts = await getRequest('product')
+        setProducts(getAllProducts.data.slice(0,8))
+    }
     return (
         <div>
             <Hero />
             <CategoriesBar setProducts={setProducts} className="mt-5" />
-            <DisplayCards />
+            <DisplayCards products={products} />
 
         </div>
     )
