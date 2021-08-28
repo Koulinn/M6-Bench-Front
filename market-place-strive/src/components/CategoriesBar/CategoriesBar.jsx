d113f1c05c8f01b80aa87803b95de553cc585699
Koulinn <drdverzola@gmail.com>
2021-08-28 15:52:53 +0100

    added categories bar

--- market-place-strive/src/components/CategoriesBar/CategoriesBar.jsx
+++ market-place-strive/src/components/CategoriesBar/CategoriesBar.jsx
@@ -1,28 +1,38 @@
 import React from 'react'
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 import { getRequest } from '../../lib/requests'
-import {Button, Container, Spinner} from 'react-bootstrap'
+import { Button, Container, Spinner } from 'react-bootstrap'
 
-function CategoriesBar() {
-    const [categories, setCategories]= useState([])
+function CategoriesBar({setProducts}) {
+    const [categories, setCategories] = useState([])
     const [isLoading, setIsLoading] = useState(true)
 
-    useEffect( async ()=>{
+    useEffect(() => {
         try {
-            const serverData = await getRequest('category')
-            setCategories(serverData.data.slice(0,9))
-            setIsLoading(false)       
+            requestData()
         } catch (error) {
             console.log(error)
         }
-    },[])
-    
+    }, [])
+
+    const requestData = async () => {
+        const serverData = await getRequest('category')
+        setCategories(serverData.data.slice(0, 9))
+        setIsLoading(false)
+    }
+
+
+
 
     return (
         <Container className="d-flex justify-content-between">
-            {isLoading? <Spinner animation='border' role='status' className="m-3"/> : <></>}
-            {!isLoading? <Button variant="secondary">All</Button> : <></>}
-            {categories.map(category=><Button key={category.id} variant="secondary">{category.category}</Button> )}  
+            {isLoading ? <Spinner animation='border' role='status' className="m-3" /> : <></>}
+            {!isLoading ? <Button variant="secondary">All</Button> : <></>}
+            {categories.map(category => {
+                return <Button key={category.id} variant="secondary" onClick={()=>setProducts({category})}
+                >{category.category}</Button>
+            }
+            )}
         </Container>
     )
 }
