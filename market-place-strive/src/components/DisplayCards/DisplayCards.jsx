09200690b5e93c323422290e9678c284710db033
Koulinn <drdverzola@gmail.com>
2021-08-28 20:53:40 +0100

    added key and filter as h2

--- market-place-strive/src/components/DisplayCards/DisplayCards.jsx
+++ market-place-strive/src/components/DisplayCards/DisplayCards.jsx
@@ -4,15 +4,15 @@ import { Card, Container } from 'react-bootstrap'
 import { getRequest } from '../../lib/requests'
 import ProdCard from '../Card/ProdCard'
 
-function DisplayCards({ products }) {
+function DisplayCards({ products, filter }) {
 
     return (
         <Container className="mt-5 cards-decker">
-            <h2>{products.category ? products.category.category : 'All'}</h2>
+            <h2>{filter}</h2>
             <div className="d-flex justify-content-between flex-wrap">
-                {products.category ? products.category.products.map(product => <ProdCard product={product} />) :
-                    products.allProducts ? products.allProducts.map(product => <ProdCard product={product} />) :
-                        products.map(product => <ProdCard product={product} />)
+                {products.category ? products.category.products.map(product => <ProdCard key={product.id} product={product} />) :
+                    products.allProducts ? products.allProducts.map(product => <ProdCard key={product.id} product={product} />) :
+                        products.map(product => <ProdCard key={product.id} product={product} />)
                 }
 
             </div>
