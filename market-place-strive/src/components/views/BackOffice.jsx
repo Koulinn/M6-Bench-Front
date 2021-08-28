111e1045f8c2d38cc0ef8b7eb1217ac1c339edc2
Koulinn <drdverzola@gmail.com>
2021-08-28 07:26:08 +0100

    input handlers working

--- market-place-strive/src/components/views/BackOffice.jsx
+++ market-place-strive/src/components/views/BackOffice.jsx
@@ -5,17 +5,31 @@ import { withRouter } from 'react-router-dom'
 import request from '../../lib/requests'
 
 function BackOffice(props) {
+    const [category, setCategory] = useState({category: ''})
+    const [product, setProduct] = useState({
+        name: '',
+        brand: '',
+        categoryId: '',
+        price: '',
+        description: '',
+        image: 'https://www.silverringsplint.com/wp-content/uploads/2018/05/Product-Image-Coming-Soon.png'
+    })
 
-    const createCategory = async (data) => {
+    const postRequest = async (endpoint, data) => {
         try {
-            const response = await request.postJSON('category', data, { "Content-Type": "application/json" })
+            const response = await request.postJSON(endpoint, data)
             console.log(response)
         } catch (error) {
             console.log(error)
         }
     }
 
-
+    const inputHandler = (key, value, action, initialValue) => {
+        action({
+            ...initialValue,
+                [key]: value
+        })
+    }
 
     return (
         <Container className="mt-5">
@@ -24,28 +38,43 @@ function BackOffice(props) {
 
                 <Form.Group as={Col} controlId="formGridName">
                     <Form.Label>Name</Form.Label>
-                    <Form.Control type="text" placeholder="Enter product Name" />
+                    <Form.Control type="text"
+                        placeholder="Enter product Name" 
+                        onChange={(e)=> inputHandler('name', e.target.value, setProduct, product)} 
+                     />
                 </Form.Group>
                 <Form.Group as={Col} controlId="formGridBrand">
                     <Form.Label>Brand</Form.Label>
-                    <Form.Control type="text" placeholder="Enter product Brand" />
+                    <Form.Control type="text" 
+                        placeholder="Enter product Brand" 
+                        onChange={(e)=> inputHandler('brand', e.target.value, setProduct, product)}
+                    />
                 </Form.Group>
 
                 <Form.Row className="px-3">
                     <Form.Group as={Col} controlId="formGridCategory">
                         <Form.Label>Category</Form.Label>
-                        <Form.Control type="text" placeholder="Category" />
+                        <Form.Control type="text"
+                             placeholder="Category"
+                             onChange={(e)=> inputHandler('category', e.target.value, setCategory, category)} 
+                         />
                     </Form.Group>
                     <Form.Group as={Col} controlId="formGridprice1">
                         <Form.Label>Price</Form.Label>
-                        <Form.Control type="number" placeholder="Price" />
+                        <Form.Control type="number" 
+                             placeholder="Price"
+                             onChange={(e)=> inputHandler('price', e.target.value, setProduct, product)} 
+                        />
                     </Form.Group>
                 </Form.Row>
 
                 <Form.Group as={Col} controlId="formGridDescription">
                     <Form.Label>Description</Form.Label>
-                    <Form.Control as="textarea" defaultValue="Description">
-                    </Form.Control>
+                    <Form.Control as="textarea"
+                         placeholder="Description"
+                        onChange={(e)=> inputHandler('description', e.target.value, setProduct, product)}
+                     />
+                   
                 </Form.Group>
 
                 <Form.Group as={Col} controlId="formGridZip">
