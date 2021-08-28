17963fdc6d695b50e78af9d01df48244b9f74cd1
Koulinn <drdverzola@gmail.com>
2021-08-28 15:52:35 +0100

    Old version was causing bugs

--- market-place-strive/src/App.js
+++ market-place-strive/src/App.js
@@ -2,13 +2,41 @@ import {Container} from 'react-bootstrap'
 import './App.css';
 import TopNavBar from './components/Navbar/TopNavBar';
 import AppRouter from './components/routes/AppRouter';
+import {
+	BrowserRouter as Router,
+	Redirect,
+	Route,
+	Switch,
+} from "react-router-dom";
+import Home from './components/views/Home';
+import ProductDetails from './components/views/ProductDetails';
+import BackOffice from './components/views/BackOffice';
+import NotFound from './components/views/NotFound';
 
 function App() {
   return (
     
     <Container className="p-0" fluid>
       <TopNavBar/>
-      <AppRouter/>
+      <Router>
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/product/:id" exact component={ProductDetails} />
+                <Route path="/backoffice" exact component={BackOffice} />
+                {/* {
+                    routes.map(route => (
+                        <Route
+                        key={route.path}
+                        path={route.path}
+                        exact={route.exact}
+                        render={route.component}
+                        />
+                    ) )
+                } */}
+                <Redirect to={"/404"} component={NotFound} />
+            </Switch>
+            
+        </Router>
     </Container>
   );
 }
