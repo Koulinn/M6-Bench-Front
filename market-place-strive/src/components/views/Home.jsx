b70d76720f3df7e3d9617113847c82e7cadc2907
Koulinn <drdverzola@gmail.com>
2021-08-28 15:53:42 +0100

    Home state depends on category chosen

--- market-place-strive/src/components/views/Home.jsx
+++ market-place-strive/src/components/views/Home.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
+import CategoriesBar from '../CategoriesBar/CategoriesBar'
 import Hero from '../Hero/Hero'
+import DisplayCards from '../DisplayCards/DisplayCards'
 
-function Home() {
+function Home(props) {
+    const [products, setProducts] = useState([])
+    
     return (
         <div>
-            <Hero/>
-            
+            <Hero />
+            <CategoriesBar setProducts={setProducts} className="mt-5" />
+            <DisplayCards />
+
         </div>
     )
 }
