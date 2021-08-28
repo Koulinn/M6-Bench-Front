e3a077533cfd759c2a82a401c2ef24794a977538
Koulinn <drdverzola@gmail.com>
2021-08-28 20:54:28 +0100

    added pagination logic

--- market-place-strive/src/components/views/Home.jsx
+++ market-place-strive/src/components/views/Home.jsx
@@ -4,26 +4,44 @@ import CategoriesBar from '../CategoriesBar/CategoriesBar'
 import Hero from '../Hero/Hero'
 import DisplayCards from '../DisplayCards/DisplayCards'
 import { getRequest } from '../../lib/requests'
+import PaginationMenu from '../PaginationMenu/PaginationMenu'
 
 function Home(props) {
     const [products, setProducts] = useState([])
+    const [filter, setFilter] = useState('all')
+    const [offset, setOffset] = useState(0)
+    const [requestDataPagination, setRequestDataPagination] = useState(false)
+    
+
     useEffect(() => {
         try {
             requestData()
         } catch (error) {
             console.log(error)
         }
-    }, [])
+    }, [requestDataPagination])
+    
+
 
     const requestData = async () => {
-        const getAllProducts = await getRequest('product')
-        setProducts(getAllProducts.data.slice(0,8))
+
+        if(filter==='all'){
+            const getAllProducts = await getRequest('product')
+            setProducts(getAllProducts.data.slice(offset, offset + 4))
+            return
+        } else {
+            const filteredProd = await getRequest(`product?offset=${offset}&filter=brand&value=${filter}&order=asc`)
+            console.log(filteredProd)
+            setProducts(filteredProd.data)
+            return
+        }
     }
     return (
         <div>
             <Hero />
-            <CategoriesBar setProducts={setProducts} className="mt-5" />
-            <DisplayCards products={products} />
+            <CategoriesBar setProducts={setProducts} setFilter={setFilter} className="mt-5" />
+            <DisplayCards products={products} filter={filter} />
+            <PaginationMenu offset={offset} setOffset={setOffset} setRequestDataPagination={setRequestDataPagination} requestDataPagination={requestDataPagination}/>
 
         </div>
     )
