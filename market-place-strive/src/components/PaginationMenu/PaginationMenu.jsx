d44c7bbd9050b027ac54ea0835b2883b8dde9428
Koulinn <drdverzola@gmail.com>
2021-08-29 09:08:10 +0100

    added useEffect to updated disable previous

--- market-place-strive/src/components/PaginationMenu/PaginationMenu.jsx
+++ market-place-strive/src/components/PaginationMenu/PaginationMenu.jsx
@@ -5,20 +5,20 @@ import { getRequest } from '../../lib/requests'
 
 function PaginationMenu({offset, setOffset, setRequestDataPagination, requestDataPagination}) {
     const [isDisabled, setIsDisabled]= useState(true)
+
+    useEffect(()=> {
+        offset === 0 ? setIsDisabled(true): setIsDisabled(false)},[offset])
     
     return (
-        <Container className="mt-5 d-flex justify-content-center">
+        <Container className="my-5 d-flex justify-content-center">
         
             <Button variant="white" className="border-secondary mr-3" disabled={isDisabled} onClick={()=>{
-                console.log('previous')
                 setOffset(offset-4)
                 setRequestDataPagination(!requestDataPagination) 
-                offset===0 ? setIsDisabled(true): setIsDisabled(false)
                 }}>Previous</Button>
             <Button variant="white" className="border-secondary ml-3" onClick={()=>{
                 setOffset(offset+4)
                 setRequestDataPagination(!requestDataPagination)
-                offset===0 ?setIsDisabled(true): setIsDisabled(false)
                 }}>Next</Button>
 
         </Container>
