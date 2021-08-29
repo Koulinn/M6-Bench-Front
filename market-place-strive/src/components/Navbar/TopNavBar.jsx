4f5c8ecd40b1612385ae8e3d4de3e7a31dd01af8
Koulinn <drdverzola@gmail.com>
2021-08-29 09:07:46 +0100

    added toggle show cart

--- market-place-strive/src/components/Navbar/TopNavBar.jsx
+++ market-place-strive/src/components/Navbar/TopNavBar.jsx
@@ -1,30 +1,26 @@
 import React from 'react'
-import {Navbar, Nav, NavDropdown, Container} from 'react-bootstrap'
+import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap'
 
-function TopNavBar() {
+function TopNavBar({toggleCart, setToggleCart}) {
     return (
-        <div>
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <div id="navBar">
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="">
                 <Container>
                     <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
                             <Nav.Link href="backoffice">Back-office</Nav.Link>
-                            <Nav.Link href="#pricing">Open cart</Nav.Link>
-                            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                            </NavDropdown>
                         </Nav>
                         <Nav>
                             <Nav.Link href="#deets">More deets</Nav.Link>
                             <Nav.Link eventKey={2} href="#memes">
                                 Dank memes
                             </Nav.Link>
+                        </Nav>
+                        <Nav className="ml-auto">
+
+                            <Nav.Link href="#" onClick={()=>setToggleCart(!toggleCart)}>Cart</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
