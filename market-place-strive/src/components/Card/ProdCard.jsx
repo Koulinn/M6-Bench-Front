454b0ad26bd66553fb0f5fbe7bb0e3a8b89b0b1d
Koulinn <drdverzola@gmail.com>
2021-08-29 09:06:37 +0100

    added post request onclick buy item

--- market-place-strive/src/components/Card/ProdCard.jsx
+++ market-place-strive/src/components/Card/ProdCard.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import {Card, Button, Badge} from 'react-bootstrap'
+import a from 'axios'
+const { post } = a
 
-function ProdCard({product}) {
+function ProdCard({product, setUpdatedCart, updateCart}) {
+
+    const addToCart = async(prodId)=>{
+        const cartRes = await post(`${process.env.REACT_APP_URL_PROD}cart/1/${prodId}`)
+
+    }
     return (
         <Card>
             <Card.Img variant="top" src={product.image} />
@@ -9,7 +16,10 @@ function ProdCard({product}) {
             <Card.Body>
                 <Card.Title>{product.name}</Card.Title>
                 <div className="d-flex justify-content-between">
-                    <Button variant="primary">Buy</Button>
+                    <Button variant="primary" onClick={()=>{
+                        addToCart(product.id)
+                        setUpdatedCart(!updateCart)
+                    }}>Buy</Button>
                     <Button variant="inlink">Details</Button>
                 </div>
             </Card.Body>
