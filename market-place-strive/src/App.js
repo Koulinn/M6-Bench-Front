aea91da6d04cd6b78f9b1109deb638e693d69877
Koulinn <drdverzola@gmail.com>
2021-08-29 09:05:51 +0100

    added changes to make shopping cart

--- market-place-strive/src/App.js
+++ market-place-strive/src/App.js
@@ -1,26 +1,32 @@
 import {Container} from 'react-bootstrap'
 import './App.css';
-import TopNavBar from './components/Navbar/TopNavBar';
-import AppRouter from './components/routes/AppRouter';
 import {
-	BrowserRouter as Router,
+  BrowserRouter as Router,
 	Redirect,
 	Route,
 	Switch,
 } from "react-router-dom";
+import TopNavBar from './components/Navbar/TopNavBar';
+import Cart from './components/Cart/Cart';
 import Home from './components/views/Home';
 import ProductDetails from './components/views/ProductDetails';
 import BackOffice from './components/views/BackOffice';
 import NotFound from './components/views/NotFound';
+import { useEffect, useState } from 'react'
+
 
 function App() {
+  const [toggleCart, setToggleCart] = useState(false)
+  const [updateCart, setUpdatedCart] = useState(false)
+
   return (
     
     <Container className="p-0" fluid>
-      <TopNavBar/>
+      <TopNavBar setToggleCart={setToggleCart} toggleCart={toggleCart}/>
+      <Cart setToggleCart={setToggleCart} toggleCart={toggleCart} setUpdatedCart={setUpdatedCart} updateCart={updateCart} />
       <Router>
             <Switch>
-                <Route path="/" exact component={Home} />
+                <Route path="/" exact render={()=><Home setUpdatedCart={setUpdatedCart} updateCart={updateCart}/>} />
                 <Route path="/product/:id" exact component={ProductDetails} />
                 <Route path="/backoffice" exact component={BackOffice} />
                 {/* {
