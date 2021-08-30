9d830d2dcc282eee6c73c6677fcf2013e73c1d5f
Koulinn <drdverzola@gmail.com>
2021-08-30 19:48:09 +0100

    cart working properly

--- market-place-strive/src/components/Hero/Hero.jsx
+++ market-place-strive/src/components/Hero/Hero.jsx
@@ -11,7 +11,7 @@ function Hero() {
     const getJumboImage = async () => {
         const data = await request.getRequest('product')
         console.log(data)
-        setHeroImg(data.data[5].image.length> 10 ? data.data[5].image : false)
+        setHeroImg( data.data[5].image && data.data[5].image.length> 10 ? data.data[5].image : false)
     }
     useEffect(()=>{
         getJumboImage()
